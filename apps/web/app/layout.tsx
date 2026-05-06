1569d81a3dd172a4207bbd45512c807d855a4756
Alexis Bouchez <git@alexisbouchez.com>
2026-05-06 10:40:27 +0200

    docs: bring AGPL v3.0 license

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,13 @@
+import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils";
+
+export const metadata: Metadata = {
+  title: "Hypersuite",
+}
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'})
 
